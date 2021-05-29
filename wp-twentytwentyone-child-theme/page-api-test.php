bafe441dc89cfc227e69be807b05263248087585
Per Thykjaer Jensen <per@multimusen.dk>
2021-05-29 14:45:39 +0200

    Update page-api-test.php

--- wp-twentytwentyone-child-theme/page-api-test.php
+++ wp-twentytwentyone-child-theme/page-api-test.php
@@ -1,14 +1,15 @@
 <?php
 /**
  * file: page-api-test
- * purpose: get blog posts via rest api on a costum page.
+ * tip: use similar code for any page, name the page page-{SLUG}.php
+ * purpose: get blog posts via rest api on a costum page accoding to your API filters.
  */
 get_header(); ?>
 
 	<!-- Posts will display below -->
 	<div id="viewPosts"></div>
 
-    <!-- REST API Script - the WP PHP "tag" will add the path of your childtheme -->
+  <!-- REST API Script - the WP PHP "tag" will add the path of your childtheme -->
 	<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/get-posts.js"></script>
 
 <?php get_footer(); ?>
