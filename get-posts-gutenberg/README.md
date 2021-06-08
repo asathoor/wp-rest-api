2f7f72d75687f976fec9e3dbd33272175eae3df1
Per Thykjaer Jensen <per@multimusen.dk>
2021-06-08 06:12:01 +0200

    The Gutenberg Mystery
    In which Sherlock Holmes and Dr. Watson detected the hidden Gutenberg styles.

---
+++ get-posts-gutenberg/README.md
@@ -0,0 +1,16 @@
+# Get Posts and Gutenberg styles
+
+How to get the Gutenberg styles is hard to find i the WordPress documentation.
+
+In this sample the styles from Twentytwentyone are copied. Here you get the feel and look of that theme. But you don't get the styles from the Gutenberg blocks.
+
+However, after adding the gutenberg-frontend.css the Gutenberg blocks seem to work as they are supposed to. The script is probably found here:
+
+https://YOUR-SITE.COM/wp-content/plugins/editorplus/dist/gutenberg-frontend.css
+
+In this theme the gutenberg-frontend.css is a copy-paste version, so if the original is updated this vervion eventually will be deprecated.
+
+## Findings
+
+* twtwone.css = optional theme styles.
+* gutenberg-frontend.css = add the blockbuilder styles.
---
+++ get-posts-gutenberg/js/get-posts-better.js
@@ -0,0 +1,36 @@
+/**
+* file: get-posts.js
+* purpose: fecth JSON via WP REST API
+**/
+
+const wpSite = "https://multimusen.dk/wpsandbox/wp-json/wp/v2/"
+const content =  viewPosts.innerHTML
+
+function fetchPosts( endpoint ) {
+
+let getPosts = wpSite + endpoint // endpoint: posts
+
+fetch(getPosts).then(
+  response => {
+    return response.json(); // get JSON data$
+  }).then(data => {
+
+  console.log(data); // what's in the JSON string?
+
+  // loop out content
+  for (let i = 0; i < data.length; i++) {
+
+    content += '<div class="posts">' // contentwrapper
+    content += '<h2>' + data[i].title.rendered + '</h2>' // title
+    content += '<div className="posts_content">'
+    content += data[i].content.rendered
+    content += '</div>' // end content
+    content += '</div>' // end contentwrapper
+
+  }
+}).catch(err => {
+  // Do something with error here
+  console.log('There was an error ...')
+})
+
+} // end fetchPostsx
