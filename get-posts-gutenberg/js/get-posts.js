99e70f5c1b7c5dce72591091393f52c168e21707
Per Thykjaer Jensen <per@multimusen.dk>
2021-06-10 14:56:32 +0200

    comments

--- get-posts-gutenberg/js/get-posts.js
+++ get-posts-gutenberg/js/get-posts.js
@@ -6,22 +6,21 @@ const getPosts = "https://multimusen.dk/wpsandbox/wp-json/wp/v2/posts" // endpoi
 
 fetch(getPosts).then(
   response => {
-    return response.json(); // get JSON data$
+    return response.json(); // get JSON data
   }).then(data => {
 
-  console.log(data); // what's in the JSON string?
+  console.log(data); // see what's in the JSON string?
 
-  // loop out something
+  // the posts array must be looped out
+  // and the data is rendered inside the HTML
   for (let i = 0; i < data.length; i++) {
-    viewPosts.innerHTML += '<div class="posts">' // contentwrapper
-    + '<h2>' + data[i].title.rendered + '</h2>' // title
+    viewPosts.innerHTML += '<div class="posts">'
+    + '<h2>' + data[i].title.rendered + '</h2>'
     + '<div className="posts_content">'
     + data[i].content.rendered
-    + '</div>' // end content
-    + '</div>' // end contentwrapper
+    + '</div>'
+    + '</div>'
   }
-
 }).catch(err => {
-  // Do something with error here
-  console.log('Error: ' + err)
+  console.log('Error: ' + err) // err will tell about the error
 })
